c2283e2bbd357d236878d300b73b46672846478a
Atsamaz Tuaev <a.tuaev@agima.ru>
2025-02-19 12:57:59 +0300

    RRPEPA-12537 правки после обновления api

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -58,8 +58,6 @@ watch(show, (newShow) => {
         background-color: #fff;
         border-radius: 30px;
         box-shadow: 0px 0px 12px 0px rgba(0,0,0,0.35);
-        height: fit-content;
-        overflow-y: auto;
         width: 100%;
         max-width: 90%;
         left: 50%;
